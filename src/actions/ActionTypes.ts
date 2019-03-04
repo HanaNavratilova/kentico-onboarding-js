75c06dae5f43570519b014c70af914edc8d5c944
Hana Navratilova <HanaN@kentico.com>
2019-03-04 15:21:34 +0100

    delete unused saveItem action type

--- src/actions/ActionTypes.ts
+++ src/actions/ActionTypes.ts
@@ -1,5 +1,4 @@
 export const AddItem = 'ADD_ITEM';
-export const SaveItem = 'SAVE_ITEM';
 export const ToggleItem = 'TOGGLE_ITEM';
 export const SetListSorting = 'SET_LIST_SORTING';
 export const SetLastRenderTime = 'SET_LAST_RENDER_TIME';
