23665726b4535fa77bf2337eec0405fa35062660
Hana Navratilova <HanaN@kentico.com>
2019-03-04 15:22:03 +0100

    delete unused addItem action type

--- src/actions/ActionTypes.ts
+++ src/actions/ActionTypes.ts
@@ -1,4 +1,3 @@
-export const AddItem = 'ADD_ITEM';
 export const ToggleItem = 'TOGGLE_ITEM';
 export const SetListSorting = 'SET_LIST_SORTING';
 export const SetLastRenderTime = 'SET_LAST_RENDER_TIME';
