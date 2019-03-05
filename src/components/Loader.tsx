f22b5c1b14e97cfb343f0776c4bf4ec8c9c4e02a
Hana Navratilova <HanaN@kentico.com>
2019-03-05 12:38:50 +0100

    add retry button and popup if loading of all buttons failed

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { ScaleLoader } from 'react-spinners';
 import { IAction } from '../actions/IAction';
 import { createErrorPopup } from '../utils/popups';
+import { FaUndo } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
 
 export interface ILoaderStateProps {
   readonly isInitialized: boolean;
@@ -17,25 +19,49 @@ interface ILoaderOwnProps {
 
 type ILoaderProps = ILoaderStateProps & ILoaderDispatchProps & ILoaderOwnProps;
 
+interface ILoaderState {
+  readonly loadingFailed: boolean;
+}
 
-export class Loader extends React.PureComponent<ILoaderProps> {
-  componentDidMount(): void {
+export class Loader extends React.PureComponent<ILoaderProps, ILoaderState> {
+  state = {
+    loadingFailed: false,
+  };
+
+  _loadItems = () => {
     this.props.onLoaderDidMount()
-      // .then()
-      .catch(_ => createErrorPopup('Loading of all items failed!'));
+      .catch(_ => {
+        createErrorPopup('Loading of all items failed!');
+        this.setState((prevState) => ({loadingFailed: !prevState.loadingFailed}));
+      });
+  };
+
+  componentDidMount(): void {
+    this._loadItems();
   }
 
+  _startLoadingItems = () => {
+    this.setState((prevState) => ({loadingFailed: !prevState.loadingFailed}));
+    this._loadItems();
+  };
+
   render(): JSX.Element {
+    const color = '#17a2b8';
+
     return (
-      <div>
-            {this.props.isInitialized
+      <div className="m-auto text-center">
+        {this.state.loadingFailed
+          ? (
+            <IconContext.Provider value={{color, size: '2em'}}>
+              <FaUndo onClick={this._startLoadingItems} />
+            </IconContext.Provider>
+          )
+          : (this.props.isInitialized
               ? this.props.children
-              : (
-                <div className="m-auto text-center">
-                  <ScaleLoader color={'#17a2b8'} />
-                </div>
-              )}
-        </div>
+              : <ScaleLoader color={color} />
+          )
+        }
+      </div>
     );
   }
 }
