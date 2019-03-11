897a04d66b86a9167fd125156a070e8199581374
Hana Navratilova <HanaN@kentico.com>
2019-03-11 15:20:03 +0100

    fix time to locale time is not used

--- src/reducers/list/item.ts
+++ src/reducers/list/item.ts
@@ -1,7 +1,7 @@
 import * as ActionType from '../../actions/ActionTypes';
 import { ListItem } from '../../models/ListItem';
 import { IAction } from '../../actions/IAction';
-import { getLocalDateTime } from '../../utils/getLocalDateTime';
+// import { getLocalDateTime } from '../../utils/getLocalDateTime';
 
 export const item = (state: ListItem = new ListItem(), action: IAction): ListItem => {
   switch (action.type) {
@@ -9,15 +9,18 @@ export const item = (state: ListItem = new ListItem(), action: IAction): ListIte
     case ActionType.FetchAddItemSucceeded:
       return new ListItem({
         ...action.payload,
-        creationTime: getLocalDateTime(action.payload.creationTime),
-        lastUpdateTime: getLocalDateTime(action.payload.lastUpdateTime)
+        creationTime: action.payload.creationTime,
+        lastUpdateTime: action.payload.lastUpdateTime
+        // creationTime: getLocalDateTime(action.payload.creationTime),
+        // lastUpdateTime: getLocalDateTime(action.payload.lastUpdateTime)
       });
 
     case ActionType.ToggleItem:
       return state.with({isActive: !state.isActive});
 
     case ActionType.FetchEditItemSucceeded: {
-      return state.with({text: action.payload.text, isActive: false, lastUpdateTime: getLocalDateTime(action.payload.lastUpdateTime)});
+      // return state.with({text: action.payload.text, isActive: false, lastUpdateTime: getLocalDateTime(action.payload.lastUpdateTime)});
+      return state.with({text: action.payload.text, isActive: false, lastUpdateTime: action.payload.lastUpdateTime});
     }
 
     default:
